20dbd0c72c9115c26fdee9000e5c19c9613510e2
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-03-22 10:20:33 +0100

    before break

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,23 @@
-import { number } from "./index";
+import { TurboNumber as TN } from "./index";
 
 describe("number", function () {
-  it("works", () => {
-    expect(number).toBe(5);
+  it("division", () => {
+    const tn = new TN(10);
+    tn.divide(5);
+    expect(tn.result()).toBe(2);
+  });
+  it("subtract", () => {
+    const tn = new TN(10);
+    tn.subtract(5);
+    expect(tn.result()).toBe(5);
+  });
+  it("division by 0", () => {
+    const tn = new TN(10);
+    expect(() => tn.divide(0)).toThrow("cannot divide by 0");
+  });
+
+  it("subtract by out of range", () => {
+    const tn = new TN(Number.MIN_SAFE_INTEGER);
+    expect(() => tn.subtract(1)).toThrow("number out of range");
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-export const number = 5;
+export class TurboNumber {
+  number: number;
+  constructor(ourNumber: number) {
+    this.number = ourNumber;
+  }
+  result() {
+    return this.number;
+  }
+  divide(num: number) {
+    this.number = this.number / num;
+  }
+  subtract(num: number) {
+    this.number = this.number - num;
+  }
+}
